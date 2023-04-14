fd7923d15735cc5680e96bd7753efc881403acf5
eemmtt <52972088+eemmtt@users.noreply.github.com>
2023-04-14 11:16:07 -0400

    moved cron business into manager, testing imgui

--- actions.py
+++ actions.py
@@ -2,7 +2,7 @@
 # courtesy of https://github.com/timo/
 #   see https://github.com/timo/talon_scripts
 # script has only been tested on Windows 10 on a single monitor
-from talon import Module, Context, cron
+from talon import Module, Context, cron, imgui, actions
 from .classes.piemenu import PieMenu
 from .classes.option import Option
 from .classes.menumanager import manager
@@ -12,66 +12,50 @@ mod = Module()
 mod.tag("pm_showing", desc="Tag indicates whether the Pie Menu is showing")
 ctx = Context()
 
-none_option = Option(label="_none", function=lambda: None)
-last_option = none_option
-pieMenu_job = None
-timestamp: float = 0
-
-def on_interval():
-    """Called every 16ms to check for user interaction with PieMenu"""
-    global last_option, timestamp, pieMenu_job
-    option = manager.get_menu_option()
-    if option.label != last_option.label:
-        last_option.focused = False
-        last_option = option
-        option.focused = True
-        timestamp = time.perf_counter()
-    if option.on_hover: 
-        option.function()
-        return
-    if option.on_dwell and time.perf_counter() - timestamp > option.dwell_time:
-        timestamp = time.perf_counter()
-        option.function()
-        return
+@imgui.open(w=100, h=100)
+def gui_test(gui: imgui.GUI):
+    gui.text(f"Hello World!")
+    gui.line()
+    gui.spacer()
+    if gui.button("Close"):
+        actions.user.piemenu_editor_close()
 
 @mod.action_class
 class PieMenuActions:     
-    def piemenu_call_and_close(option: Option = none_option):
+    def piemenu_call_and_close():
         """Calls the selected function and closes the menu"""
         if not "user.pm_showing" in ctx.tags:
             return
-        global pieMenu_job, last_option
-        cron.cancel(pieMenu_job)
-        manager.close_menu()
         ctx.tags = []
-        
-        option = manager.get_menu_option()
-        option.function()
-        option.focused = False
-        last_option = none_option
+        manager.close_menu()
         
     def piemenu_launch(screen: int, layer: int = 0):
         """Launches Pie Menu"""
-        manager.set_menu(layer)
+        if "user.pm_showing" in ctx.tags:
+            return
         ctx.tags = ["user.pm_showing"]
-        
-        global pieMenu_job
-        pieMenu_job = cron.interval("16ms", on_interval)
+        manager.launch_menu(layer)
     
     def piemenu_toggle(screen: int, layer: int = 0):
         """Toggles Pie Menu"""
         global pieMenu_job, last_option
         if "user.pm_showing" in ctx.tags:
-            cron.cancel(pieMenu_job)
-            manager.close_menu()
             ctx.tags = []
-            
-            option = manager.get_menu_option()
-            option.function()
-            option.focused = False
-            last_option = none_option
+            manager.close_menu()
         else:
-            manager.set_menu(layer)
             ctx.tags = ["user.pm_showing"]
-            pieMenu_job = cron.interval("16ms", on_interval)
+            manager.launch_menu(layer)
+            
+    def piemenu_editor_show():
+        """Show Pie Menu Editor"""
+        print("Showing Pie Menu Editor")
+        gui_test.show()
+        pass
+    
+    def piemenu_editor_close():
+        """Close Pie Menu Editor"""
+        print("Closing Pie Menu Editor")
+        gui_test.hide()
+        pass
+        
 
