511412381a08b8f307117f5faa572ffec79c9eb2
eemmtt <etmuzio@gmail.com>
2024-08-09 14:19:55 -0400

    comment out Option methods

--- classes/option.py
+++ classes/option.py
@@ -21,26 +21,28 @@ class Option:
 
 
 # ------ Functions for use with Option.function ------ #
+# *functions called by Options are currently implemented in the menumanager class!
 
-def shout(text: str):
-        def shout():
-            print(f"shouting {text}!")
-        return shout
+
+# def shout(text: str):
+#         def shout():
+#             print(f"shouting {text}!")
+#         return shout
     
-def scroll(x: int = 0, y: int = 0):
-    def scroll():
-        actions.mouse_scroll(y=y, x=x)
-    return scroll
+# def scroll(x: int = 0, y: int = 0):
+#     def scroll():
+#         actions.mouse_scroll(y=y, x=x)
+#     return scroll
 
-def insert(text: str):
-    def insert():
-        actions.insert(text)
-    return insert
+# def insert(text: str):
+#     def insert():
+#         actions.insert(text)
+#     return insert
 
-def key(key: str):
-    def keypress():
-        actions.key(key=key)
-    return keypress
+# def key(key: str):
+#     def keypress():
+#         actions.key(key=key)
+#     return keypress
 
 # Dont use this, need to implement held keys at option level
 # def f_key_press_hold(key: str):
@@ -48,13 +50,13 @@ def key(key: str):
 #         ctrl.key_press(key=key, down=True)
 #     return keypress
 
-def printAppName(self):
-    def printAppName():
-        print(ui.active_app().name)
-    return printAppName
+# def printAppName(self):
+#     def printAppName():
+#         print(ui.active_app().name)
+#     return printAppName
 
-def macro(*functions):
-    def macro():
-        for function in functions:
-            function()
-    return macro
+# def macro(*functions):
+#     def macro():
+#         for function in functions:
+#             function()
+#     return macro
--- piemenu.talon
+++ piemenu.talon
@@ -3,8 +3,8 @@
 
 #key(f24): user.piemenu("toggle")
 #key(f24): user.piemenu_editor("open")
-key(f24:down): user.piemenu("open")
-key(f24:up): user.piemenu("close")
-key(ctrl-alt-a): user.piemenu("toggle")
-# key(ctrl-alt-a:down): user.piemenu("open")
-#key(ctrl-alt-a:up): user.piemenu("close")
+#key(f24:down): user.piemenu("open")
+#key(f24:up): user.piemenu("close")
+#key(ctrl-alt-a): user.piemenu("toggle")
+key(ctrl-alt-a:down): user.piemenu("open")
+key(ctrl-alt-a:up): user.piemenu("close")
